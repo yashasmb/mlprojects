3ce10452b8e4540b27341a88fc537aece8394633
Yashas M B <yashasmb2003@gmail.com>
2025-07-15 15:56:19 +0530

    deployment configuration

--- src/utils.py
+++ src/utils.py
@@ -66,3 +66,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params,cv=3,n_jobs=-1,v
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+
+def load_obj(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            return dill.load(file)
+    except Exception as e:
+        raise CustomException(e, sys)
